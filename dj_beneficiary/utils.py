f8271ae3b97a44d333e5cf39367f0e6c4024834e
Jachin Manda <jachin.dev@outlook.com>
2021-12-29 10:26:53 +0200

    Add a reusable uuid generator funtion.

---
+++ dj_beneficiary/utils.py
@@ -0,0 +1,28 @@
+import uuid
+
+
+def generate_uuid(chars=None, length=8, upper=True):
+    """
+    Generates a UUID string.
+
+    Args:
+        chars (str, optional): A string to be appended to the UUID string. Defaults to None.
+        length (int, optional): The length of the UUID string. Defaults to 8.
+        upper (bool, optional): Specify whether the UUID characters should be uppercase or lower case. Defaults to False.
+
+    Return:
+        string
+    """
+    uuid_chars = str(uuid.uuid4())[:length]
+
+    if upper:
+        uuid_chars = uuid_chars.upper()
+
+    complete_string = ""
+
+    if chars:
+        complete_string = f"{chars}-{uuid_chars.upper()}"
+    else:
+        complete_string = uuid_chars
+
+    return complete_string
