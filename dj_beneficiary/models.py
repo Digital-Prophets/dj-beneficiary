a780c1d34839b498b3a1450dffedffe8e9d7c92d
Jachin Manda <jachin.dev@outlook.com>
2021-12-29 23:37:45 +0200

    Refactor models to be used as abstract base models.

--- dj_beneficiary/models.py
+++ dj_beneficiary/models.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 from imagekit.processors import ResizeToFill
 from imagekit.models import ProcessedImageField
 
-class Province(models.Model):
+class BaseProvinceAbstract(models.Model):
     """
     Implements province properties and appropriate methods.
     """
@@ -20,11 +20,10 @@ class Province(models.Model):
     def __str__(self):
         return self.name
     class Meta:
-        verbose_name = "Province"
-        verbose_name_plural = "Provinces"
-        ordering = ["-created"]
+        abstract = True
 
-class District(models.Model):
+
+class BaseDistrictAbstract(models.Model):
     """
     Define district properties and appropriate methods.
     """
@@ -33,17 +32,16 @@ class District(models.Model):
         _("District"),
         max_length=255
     )
-    province = models.ForeignKey(
-        Province,
-        on_delete=models.CASCADE,
-    )
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
 
 
-class Ward(models.Model):
+class BaseWardAbstract(models.Model):
     """
     Defines a Ward and all its approriate implemeentation methods.
     """
@@ -53,12 +51,11 @@ class Ward(models.Model):
         max_length=255
     )
 
-    district = models.ForeignKey(
-        District,
-        on_delete=models.CASCADE,
-    )
 
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
@@ -103,19 +100,12 @@ class FacilityType(models.Model):
     def __str__(self):
         return self.name.title()
 
-class Facility(models.Model):
+class BaseFacilityAbstract(models.Model):
     hmis_code = models.CharField(
         _('HMIS Code'),
         max_length=100,
         null=True,
         blank=True
-    )
-    district = models.ForeignKey(
-        District,
-        default=1,
-        on_delete=models.CASCADE,
-        help_text=_("District in which the facility is located"),
-        max_length=250,
     )
     name = models.CharField(
         _('Name'),
@@ -140,15 +130,14 @@ class Facility(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Facility'
-        verbose_name_plural = 'Facilities'
+        abstract = True
 
     def save(self):
         if self.facility_type.name.lower() in self.name.lower():
             self.name = (self.name).title()
         else:
             f"{self.name.title()} {self.facility_type.name.title()}"
-        super(Facility, self).save()
+        super(BaseFacilityAbstract, self).save()
 
     def __str__(self):
         return str(self.name)
@@ -165,20 +154,17 @@ SEX_CHOICES = (
     ("Female", _("Female"))
 )
 
-class Agent(models.Model):
+class BaseAgentAbstract(models.Model):
     """
     Create agent detail table with its attributes or columns.
     """
     first_name = models.CharField(
         _("First Name"),
         max_length=200,
-        null=True,
-        blank=True
     )
     last_name = models.CharField(
         _("Last Name"),
         max_length=200,
-        null=False
     )
     birthdate = models.DateField(
         _("Birth Date"),
@@ -196,18 +182,15 @@ class Agent(models.Model):
         choices=GENDER_CHOICES,
         default=GENDER_CHOICES[3][0]
     )
-    # location = models.PointField(
-    #     _("Location"),
-    #     geography=True,
-    #     blank=True,
-    #     null=True,
-    #     srid=4326
-    # )
+
+    class Meta:
+        # NOTE: This means we can not reuse this model directly but subclass it with inheritance and add extra fields or override existing fields
+        abstract = True 
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-class IndividualBeneficiary(models.Model):
+class BaseIndividualBeneficiaryAbstract(models.Model):
     """
     Implements the Beneficiary object as an individual Person.
     """
@@ -315,19 +298,6 @@ class IndividualBeneficiary(models.Model):
         null=True,
         blank=True,
     )
-    agent = models.ForeignKey(
-        Agent,
-        on_delete=models.PROTECT,
-        null=True,
-        blank=True
-    )
-    registered_facility = models.ForeignKey(
-        'Facility',
-        on_delete=models.PROTECT,
-        null=True,
-        blank=True,
-        related_name="registerd_facility"
-    )
     date_of_birth = models.DateField(_("Date of Birth"))
     marital_status = models.CharField(
         _("Marital Status"),
@@ -367,9 +337,7 @@ class IndividualBeneficiary(models.Model):
     alive = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     class Meta:
-        verbose_name = "Beneficiary Person"
-        verbose_name_plural = "Beneficiary Persons"
-        ordering = ["created"]
+        abstract = True
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -383,7 +351,7 @@ class IndividualBeneficiary(models.Model):
         return age
 
 
-class OrganizationBeneficiary(models.Model):
+class BaseOrganizationBeneficiaryAbstract(models.Model):
     """
     Implements the Beneficiary object an organizational entity.
     i.e co-oroperative, business firm etc.
@@ -403,12 +371,6 @@ class OrganizationBeneficiary(models.Model):
         _("Organization Type"),
         max_length=100,
         choices=BENEFICIARY_ORGANIZATION_TYPE,
-    )
-    ward = models.ForeignKey(
-        Ward, 
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True
     )
     logo = ProcessedImageField(
         upload_to='org_logos',
@@ -489,9 +451,6 @@ class OrganizationBeneficiary(models.Model):
     class Meta:
         # NOTE: This means we can not reuse this model directly but subclass it with inheritance and add extra fields or override existing fields
         abstract = True 
-        verbose_name = "Beneficiary Organization"
-        verbose_name_plural = "Beneficiary Organizations"
-        ordering = ["created"]
 
     def __str__(self):
         return f"{self.name}"
